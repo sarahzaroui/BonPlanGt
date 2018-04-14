06b107081ed0dd2419a28697824cf8df210affe9
alaindin <alaaeddine.hfidhi@esprit.tn>
2018-04-14 01:14:42 +0100

    update entitée Article

--- src/Front/BonPlanBundle/Entity/Article.php
+++ src/Front/BonPlanBundle/Entity/Article.php
@@ -24,22 +24,36 @@ class Article
     /**
      * @var string
      *
-     * @ORM\Column(name="nom", type="string", length=254, nullable=true)
+     * @ORM\Column(name="titre", type="string", length=254, nullable=true)
      */
-    private $nom;
+    private $titre;
 
     /**
      * @var string
      *
-     * @ORM\Column(name="description", type="string", length=254, nullable=true)
+     * @ORM\Column(name="description", type="string", length=500, nullable=true)
      */
     private $description;
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="imagename", type="string", length=500, nullable=true)
+     */
+    private $imagename;
 
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="adresse", type="string", length=500, nullable=true)
+     */
+
+    private $adresse;
     /**
      * @var \DateTime
      *
      * @ORM\Column(name="date", type="datetime", nullable=true)
      */
+
     private $date;
 
     /**
@@ -79,7 +93,167 @@ class Article
      */
     private $iduser;
 
-    //test
+    /**
+     * @return int
+     */
+    public function getIdarticle()
+    {
+        return $this->idarticle;
+    }
+
+    /**
+     * @param int $idarticle
+     */
+    public function setIdarticle($idarticle)
+    {
+        $this->idarticle = $idarticle;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitre()
+    {
+        return $this->titre;
+    }
+
+    /**
+     * @param string $titre
+     */
+    public function setTitre($titre)
+    {
+        $this->titre = $titre;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return string
+     */
+    public function getImagename()
+    {
+        return $this->imagename;
+    }
+
+    /**
+     * @param string $imagename
+     */
+    public function setImagename($imagename)
+    {
+        $this->imagename = $imagename;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAdresse()
+    {
+        return $this->adresse;
+    }
+
+    /**
+     * @param string $adresse
+     */
+    public function setAdresse($adresse)
+    {
+        $this->adresse = $adresse;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * @param \DateTime $date
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEtat()
+    {
+        return $this->etat;
+    }
+
+    /**
+     * @param string $etat
+     */
+    public function setEtat($etat)
+    {
+        $this->etat = $etat;
+    }
+
+    /**
+     * @return \Categoriearticle
+     */
+    public function getIdcatart()
+    {
+        return $this->idcatart;
+    }
+
+    /**
+     * @param \Categoriearticle $idcatart
+     */
+    public function setIdcatart($idcatart)
+    {
+        $this->idcatart = $idcatart;
+    }
+
+    /**
+     * @return \Region
+     */
+    public function getIdregion()
+    {
+        return $this->idregion;
+    }
+
+    /**
+     * @param \Region $idregion
+     */
+    public function setIdregion($idregion)
+    {
+        $this->idregion = $idregion;
+    }
+
+    /**
+     * @return \Client
+     */
+    public function getIduser()
+    {
+        return $this->iduser;
+    }
+
+    /**
+     * @param \Client $iduser
+     */
+    public function setIduser($iduser)
+    {
+        $this->iduser = $iduser;
+    }
+
+   
 
 }
 
